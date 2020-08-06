66fcd7db2bff4fc1518ccfc49e9db42a6221d249
Imraan-Hendricks <imraan.hndx@gmail.com>
2020-08-07 01:14:49 +0200

    added sections component styles

--- src/components/sections.jsx
+++ src/components/sections.jsx
@@ -1,15 +1,16 @@
 import React from 'react';
+import cssModule from '../styles/sections.module.scss';
 
 const Sections = () => {
   return (
     <div>
-      <div>
+      <div className={[cssModule.section, cssModule.sectionGreen].join(' ')}>
         <p>Section 1</p>
       </div>
-      <div>
+      <div className={cssModule.section}>
         <p>Section 2</p>
       </div>
-      <div>
+      <div className={[cssModule.section, cssModule.sectionGreen].join(' ')}>
         <p>Section 3</p>
       </div>
     </div>
