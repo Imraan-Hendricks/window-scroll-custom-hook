5b0fec36988e5121cc937b253d9ffa8a9dd06d14
Imraan-Hendricks <imraan.hndx@gmail.com>
2020-08-07 01:22:05 +0200

    added scroll effects to sections component

--- src/components/sections.jsx
+++ src/components/sections.jsx
@@ -1,16 +1,26 @@
 import React from 'react';
 import cssModule from '../styles/sections.module.scss';
+import useWindowScroll from '../hooks/window-scroll';
 
 const Sections = () => {
+  const { top } = useWindowScroll();
+
+  const styles = () => {
+    if (top) {
+      return { bg: cssModule.sectionGreen };
+    }
+    return { bg: cssModule.sectionBlue };
+  };
+
   return (
     <div>
-      <div className={[cssModule.section, cssModule.sectionGreen].join(' ')}>
+      <div className={[cssModule.section, styles().bg].join(' ')}>
         <p>Section 1</p>
       </div>
       <div className={cssModule.section}>
         <p>Section 2</p>
       </div>
-      <div className={[cssModule.section, cssModule.sectionGreen].join(' ')}>
+      <div className={[cssModule.section, styles().bg].join(' ')}>
         <p>Section 3</p>
       </div>
     </div>
