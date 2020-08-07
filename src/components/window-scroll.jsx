b9f288921873422c340303edc3da2e4ef1ade53f
Imraan-Hendricks <imraan.hndx@gmail.com>
2020-08-08 01:17:52 +0200

    added window scroll component styles

--- src/components/window-scroll.jsx
+++ src/components/window-scroll.jsx
@@ -1,28 +1,46 @@
 import React from 'react';
+import cssModule from '../styles/window-scroll.module.scss';
 import useWindowScroll from '../hooks/window-scroll';
 
 const WindowScroll = () => {
   const { pageYOffset, scrollDirection, top } = useWindowScroll();
 
+  const styles = () => {
+    if (top) {
+      return {
+        h1: cssModule.h1Top,
+        span: cssModule.spanTop,
+        hr: cssModule.hrTop,
+      };
+    }
+    return {
+      h1: cssModule.h1,
+      span: cssModule.span,
+      hr: cssModule.hr,
+    };
+  };
+
   return (
-    <div>
-      <h1>
+    <div className={cssModule.root}>
+      <h1 className={styles().h1}>
         Scroll <br />
         information:
       </h1>
       <small>Scroll to update information</small>
-      <hr />
+      <hr className={styles().hr} />
       <p>
-        <span>pageYOffset:</span>
+        <span className={styles().span}>pageYOffset:</span>
         <br />
         {pageYOffset}
       </p>
       <p>
-        <span>scrollDirection:</span> {scrollDirection && scrollDirection}
+        <span className={styles().span}>scrollDirection:</span>{' '}
+        {scrollDirection && scrollDirection}
         {!scrollDirection && 'undefined'}
       </p>
       <p>
-        <span>top:</span> {top && 'true'} {!top && 'false'}
+        <span className={styles().span}>top:</span> {top && 'true'}{' '}
+        {!top && 'false'}
       </p>
     </div>
   );
